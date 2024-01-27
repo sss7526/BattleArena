2765fae36004b5248d054d377aff440b74224b96
sss7526 <snor190@wgu.edu>
2024-01-27 18:02:28 -0500

    Dice update from subuof

--- Dice/Game.java
+++ Dice/Game.java
@@ -1,9 +1,11 @@
 import java.util.Random;
 import java.util.Scanner;
-// comment added lol
+
 public class Game {
-
-
+    private final Random rand = new Random();
+    private final Scanner scanner = new Scanner(System.in);
+    private Player friend;
+    private Player you;
 
     public static class Player {
         private int cash;
@@ -20,32 +22,35 @@ public class Game {
         }
     }
 
-    private final Random rand = new Random();
-    private final Scanner scanner = new Scanner(System.in);
-    private final Player you;
-    private final Player friend;
-
     public Game() {
         System.out.println("While walking to your CS class, your degenerate friend entices you to a game of dice. What do you do?");
         System.out.println();
         System.out.println("1. Continue to class like aresponsible adult.");
         System.out.println("2. Play like the degenerate you are.");
-        System.out.print("\nSelect 1 or 2");
+       // System.out.print("\nSelect 1 or 2: ");
     }
     
     public void start() {
-        switch (scanner.nextLine()) {
-            case "1":
-                System.out.print("Enter your name: ");
-                you = new Player(scanner.nextLine(), rand);
-                System.out.print("Enter your freind's name: ");
-                friend = new Player(scanner.nextLine(), rand);
-            case "2":
-                System.out.println("NERD!!!");
-                System.exit(0);
-            default:
-                System.err.println("Invalid option");
-        }
+	boolean stop = true;
+	while (stop) {
+        	System.out.print("\nSelect 1 or 2: ");
+		switch (scanner.nextLine()) {
+            		case "1":
+                		System.out.println("NERD!!!");
+                		System.exit(0);
+            		case "2":
+                		System.out.print("Enter your name: ");
+                		you = new Player(scanner.nextLine(), rand);
+                		System.out.print("Enter your freind's name: ");
+                		friend = new Player(scanner.nextLine(), rand);
+				stop = false;
+				break;
+            		default:
+                		System.err.println("Invalid option");
+        	}
+	}
+
+	run();
     }
     
     public void run() {
